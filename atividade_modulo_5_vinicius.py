682c988d266d5a1369c5e2f2db1844cc0061a28e
viniciusemvocacao <euvinicius2011@gmail.com>
2026-03-18 11:55:41 -0300

    Add files via upload

---
+++ atividade_modulo_5_vinicius.py
@@ -0,0 +1,13 @@
+def realizar_login():
+
+     
+     print('\nTela de login - Meet\n')
+     
+     nome_usuario = input('Digite seu login para continuar: ')
+     senha_usuario = input('Digite sua senha para continuar: ')
+     
+     print(f'Bem-vindo, {nome_usuario}!')
+     
+     print('----------------------------\n')
+
+realizar_login()
