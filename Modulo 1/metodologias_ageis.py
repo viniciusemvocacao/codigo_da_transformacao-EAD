906b58963e386cb7b96c9d17e41121b427fb0264
viniciusemvocacao <euvinicius2011@gmail.com>
2026-04-14 08:32:30 -0300

    Add files via upload

---
+++ Modulo 1/metodologias_ageis.py
@@ -0,0 +1,11 @@
+print("--- Sistema de Agendamento Simples ---")
+
+cliente = input("Nome do cliente: ")
+servico = input("Tipo de serviço: ")
+data_hora = input("Data e horário (ex: 15/10 às 14h): ")
+
+print("\n--- Agendamento Confirmado ---")
+print(f"Cliente: {cliente}")
+print(f"Serviço: {servico}")
+print(f"Horário: {data_hora}")
+print("------------------------------")
