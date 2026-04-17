906f1f2b63f04df15e723f04c3147b23da860ae0
viniciusemvocacao <euvinicius2011@gmail.com>
2026-04-17 10:31:31 -0300

    update

--- Modulo 5/media_aluno.py
+++ Modulo 5/media_aluno.py
@@ -7,3 +7,7 @@ nota2 = float(input("Digite a segunda nota: "))
 nota3 = float(input("Digite a terceira nota: "))
 media = calcular_media(nota1, nota2, nota3)
 print(f"A média do aluno é: {media}")
+if media >= 6:
+    print("Aluno aprovado!")
+else:   
+    print("Aluno reprovado!")
